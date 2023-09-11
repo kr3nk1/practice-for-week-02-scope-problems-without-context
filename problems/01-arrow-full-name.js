fdbfb2f045a43dfd5e9f6a506dabb96935119044
Josué Garrido <jv.garrido99@gmail.com>
2023-09-11 15:18:22 -0300

    npm installed

--- problems/01-arrow-full-name.js
+++ problems/01-arrow-full-name.js
@@ -14,7 +14,12 @@ arrowGetFullName(p2); // => 'Charlie Brown'
 
 ***********************************************************************/
 
-// Your code here
+const arrowGetFullName = person => {person[firstName] + " " + person[lastName]};
+
+let p1 = {firstName: 'John', lastName: 'Doe'};
+arrowGetFullName(p1); // => 'John Doe'
+let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
+arrowGetFullName(p2); // => 'Charlie Brown'
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
